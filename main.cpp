6c61f87b647c64b479ede04891ec66db8c3c1d5f
angelobrajuka <brajuka.angelo@gmail.com>
2020-07-21 18:51:28 -0400

    updated test, works with multiple data types in 1 file

--- main.cpp
+++ main.cpp
@@ -10,13 +10,20 @@ int main() {
     i[1] = 245349070;
     i[2] = 98765432;
     writeIntsToFile(ofs, i, 3);
+    char x[2] = {0};
+    x[0] = 'a';
+    x[1] = 'B';
+    writeCharsToFile(ofs, x, 2);
     closeStream(ofs);
 
     std::ifstream ifs;
     openStream(ifs, "file.sav");
     int32_t j[3];
     readIntsFromFile(ifs, j, 3);
+    char y[2];
+    readCharsFromFile(ifs, y, 2);
     closeStream(ifs);
 
     std::cout << j[0] << " " << j[1] << " " << j[2] << std::endl;
+    std::cout << y[0] << " " << y[1] << std::endl;
 }
