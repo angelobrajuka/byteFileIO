686b5f11fb930069a84566d2e1a01e599a73595b
angelobrajuka <brajuka.angelo@gmail.com>
2020-07-23 14:01:49 -0400

    made simpler

--- byteFileIO.h
+++ byteFileIO.h
@@ -7,6 +7,9 @@
 
 namespace byteFileIO {
     namespace {
+        std::ofstream ofs;
+        std::ifstream ifs;
+
         char getByte(int i, uint8_t byte) {
             return (uint8_t)(i >> (byte*8));
         }
@@ -16,51 +19,51 @@ namespace byteFileIO {
         }
     }
     
-    void openStream(std::ifstream &ifs, const char *name) {
+    void openifs(const char *name) {
         ifs.open(name, std::ios::binary);
         ifs >> std::noskipws;
     }
 
-    void openStream(std::ofstream &ofs, const char *name) {
+    void openofs(const char *name) {
         ofs.open(name, std::ios::binary);
     }
 
-    void closeStream(std::ifstream &ifs) {
+    void closeifs() {
         ifs.close();
     }
 
-    void closeStream(std::ofstream &ofs) {
+    void closeofs() {
         ofs.close();
     }
 
-    void writeCharsToFile(std::ofstream &ofs, char chars[], int length) {
+    void writeCharsToFile(char chars[], int length) {
         for(int i = 0; i < length; i++) {
             ofs << chars[i];
             //std::cout << "charout: " << (int)chars[i] << std::endl;
         }
     }
 
-    void writeIntsToFile(std::ofstream &ofs, int32_t ints[], int length) {
+    void writeIntsToFile(int32_t ints[], int length) {
         char chars[length*sizeof(int32_t)];
         for(int i = 0; i < length; i++) {
             for(int j = 0; j < sizeof(int32_t); j++) {
                 chars[i*4+j] = getByte(ints[i], j);
             }
         }
-        writeCharsToFile(ofs, chars, length*sizeof(int32_t));
+        writeCharsToFile(chars, length*sizeof(int32_t));
     }
 
-    void writeFloatsToFile(std::ofstream &ofs, float floats[], int length) {
+    void writeFloatsToFile(float floats[], int length) {
         char chars[length*sizeof(float)];
         for(int i = 0; i < length; i++) {
             for(int j = 0; j < sizeof(float); j++) {
                 chars[i*4+j] = getByte(floats[i], j);
             }
         }
-        writeCharsToFile(ofs, chars, length*sizeof(float));
+        writeCharsToFile(chars, length*sizeof(float));
     }
 
-    void readCharsFromFile(std::ifstream &ifs, char chars[], int length) {
+    void readCharsFromFile(char chars[], int length) {
         for(int i = 0; i < length; i ++) {
             chars[i] = (int8_t)0x00;
             ifs >> chars[i];
@@ -68,9 +71,9 @@ namespace byteFileIO {
         }
     }
 
-    void readIntsFromFile(std::ifstream &ifs, int32_t ints[], int length) {
+    void readIntsFromFile(int32_t ints[], int length) {
         char chars[length*sizeof(int32_t)];
-        readCharsFromFile(ifs, chars, length*sizeof(int32_t));
+        readCharsFromFile(chars, length*sizeof(int32_t));
         for(int i = 0; i < length; i ++) {
             ints[i] = 0x00;
             for(uint8_t j = 0; j < sizeof(int32_t); j ++) {
@@ -79,9 +82,9 @@ namespace byteFileIO {
         }
     }
 
-    void readFloatsFromFile(std::ifstream &ifs, float floats[], int length) {
+    void readFloatsFromFile(float floats[], int length) {
         char chars[length*sizeof(float)];
-        readCharsFromFile(ifs, chars, length*sizeof(float));
+        readCharsFromFile(chars, length*sizeof(float));
         for(int i = 0; i < length; i ++) {
             floats[i] = *((float *)&chars[i*4]);
         }
--- main.cpp
+++ main.cpp
@@ -3,42 +3,39 @@
 using namespace byteFileIO;
 
 int main() {
-    // std::ofstream ofs;
-    // openStream(ofs, "file.sav");
-    // int32_t i[3] = {0x00};
-    // i[0] = 12345678;
-    // i[1] = 245349070;
-    // i[2] = 98765432;
-    // writeIntsToFile(ofs, i, 3);
-    // char x[2] = {0};
-    // x[0] = 'a';
-    // x[1] = 'B';
-    // writeCharsToFile(ofs, x, 2);
-    // closeStream(ofs);
-
-    // std::ifstream ifs;
-    // openStream(ifs, "file.sav");
-    // int32_t j[3];
-    // readIntsFromFile(ifs, j, 3);
-    // char y[2];
-    // readCharsFromFile(ifs, y, 2);
-    // closeStream(ifs);
-
-    // std::cout << j[0] << " " << j[1] << " " << j[2] << std::endl;
-    // std::cout << y[0] << " " << y[1] << std::endl;
-    // std::cout << sizeof(float) << std::endl;
-
-    std::ofstream ofs;
-    openStream(ofs, "file.sav");
-    float f[4] = {0.0, 0.1, 0.2, 5.1};
-    writeFloatsToFile(ofs, f, 4);
-    closeStream(ofs);
+    openofs("file.sav");
+    int32_t i[3] = {0x00};
+    i[0] = 12345678;
+    i[1] = 245349070;
+    i[2] = 98765432;
+    writeIntsToFile(i, 3);
+    char x[2] = {0};
+    x[0] = 'a';
+    x[1] = 'B';
+    writeCharsToFile(x, 2);
+    closeofs();
 
     std::ifstream ifs;
-    openStream(ifs, "file.sav");
+    openifs("file.sav");
+    int32_t j[3];
+    readIntsFromFile(j, 3);
+    char y[2];
+    readCharsFromFile(y, 2);
+    closeifs();
+
+    std::cout << j[0] << " " << j[1] << " " << j[2] << std::endl;
+    std::cout << y[0] << " " << y[1] << std::endl;
+    std::cout << sizeof(float) << std::endl;
+
+    openofs("file.sav");
+    float f[4] = {0.0, 0.1, 0.2, 5.1};
+    writeFloatsToFile(f, 4);
+    closeofs();
+
+    openifs("file.sav");
     float f2[4];
-    readFloatsFromFile(ifs, f2, 4);
-    closeStream(ifs);
+    readFloatsFromFile(f2, 4);
+    closeifs();
     
     for(int i = 0; i < 4; i ++) {
         std::cout << f2[i] << std::endl;
